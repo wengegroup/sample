ee5c6864b6213b55771cf6bc7b2b1b8aeccb80ca
wengegroup <bao.hao@wengegroup.com>
2020-02-07 20:28:18 +0800

    update

--- src/main/java/com/wenge/api/config/GetToken.java
+++ src/main/java/com/wenge/api/config/GetToken.java
@@ -1,28 +1,23 @@
 package com.wenge.api.config;
 
-import com.wenge.api.test.RequestUtils;
-import net.sf.json.JSONObject;
-
-/**
- * @author lzk
- * @description
- * @date 2019/7/15
- */
 public class GetToken {
-    public String getAnalysisToken(){
+
+    /**分析接口，获取token**/
+    public void getAnalysisToken(){
+        String path = "/openAPI/analysis/getAccessToken";
+        String method = "POST";
+        String authen_origion_code = "用户的user Code" + "_" + System.currentTimeMillis();
+        String result = RequestUtils.request("WH_Analys_ECRule", path, method,authen_origion_code);
+        System.out.println("result is"+result);
+
+    }
+
+    /**搜索接口，获取token**/
+    public void getSearchToken(){
         String path = "/openAPI/search/getAccessToken";
         String method = "POST";
-        // 示例   d0u4oE0n4iFu8911D39A0C_1579680932844
-        String authen_origion_code = "";// + System.currentTimeMillis();
-        System.out.println("【客户端认证信息】----" + authen_origion_code);
-        // AES加密规则
+        String authen_origion_code = "用户的user Code" + "_" + System.currentTimeMillis();
         String result = RequestUtils.request("WH_Search_ECRule", path, method,authen_origion_code);
         System.out.println(result);
-        JSONObject outObject2 = JSONObject.fromObject(result);
-        String aToken = "";
-        if(outObject2 != null){
-            aToken = outObject2.get("access_token").toString();
-        }
-        return aToken;
     }
 }
--- src/main/java/com/wenge/api/config/RequestConfig.java
+++ src/main/java/com/wenge/api/config/RequestConfig.java
@@ -1,22 +1,10 @@
 package com.wenge.api.config;
 
-/**
- * @author lzk
- * @description
- * @date 2019/7/15
- */
 public class RequestConfig {
     //请求域名
     public static final String ONLINE_HOST = "http://wenhai.wengegroup.com/wenhaiFt-api";   // official
-    public static final String ONLINE_HOST_test = "http://ft.wengetech.com:10001/wenhaiFt-api-test"; //test
-    public static final String HOST = "http://192.168.10.193:8010/wenhaiFt-api";
-    public static final String LOCAL_HOST = "http://localhost:8090";
 
     //用户使用code请求的access token
     public static final String ANALYSIS_ACCESS_TOKEN_INFO = "240b850930799b7389ef4ce5089e4018";
-
     public static final String SEARCH_ACCESS_TOKEN_INFO = "ace8d6fdd1aa178091c2577fe7210ae3";
-
-    public static final String YANGSHI_ACCESS_TOKEN_INFO = "4b2f9c7f9eecc8384b79a08e75ce7957";
-
 }
